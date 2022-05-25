845175775c7a16335ec1e7a05db322ccb8ce51a9
quocthangK64 <thang.nq194839@sis.hust.edu.vn>
2022-05-25 17:10:09 +0700

    ADD: other sub-component for FlightSearchBox

---
+++ travelwala/src/pages/home/components/LocationBox.js
@@ -0,0 +1,50 @@
+import * as React from 'react'
+import {forwardRef, useState} from 'react'
+import {MenuItem} from "@mui/material";
+import Box from "@mui/material/Box";
+import IconTextField from "../../../components/IconTextField";
+
+function LocationBox(props, ref) {
+  const [airport, setAirport] = useState(props.title === "From" ? 'Noi Bai' : 'Tan Son Nhat');
+  const handleChange = (event) => {
+    setAirport(event.target.value);
+  };
+  return (<div>
+      <h2 style={{marginBottom: 10}}>{props.title}</h2>
+      <Box
+        ref={ref}
+        component="form"
+        sx={{
+          '& .MuiTextField-root': {m: 1, width: '300px'},
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <IconTextField
+          id={props.title}
+          select
+          label="Destination"
+          value={airport}
+          onChange={handleChange}
+          iconStart={props.iconStart}
+        >
+          {airports.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.city}
+            </MenuItem>
+          ))}
+        </IconTextField>
+      </Box>
+    </div>
+  );
+}
+
+const airports = [
+  {label: 'Noi Bai', city: 'Hanoi', value: 'Noi Bai'},
+  {label: 'Tan Son Nhat', city: 'Ho Chi Minh City', value: 'Tan Son Nhat'},
+  {label: 'Cam Ranh', city: 'Nha Trang', value: 'Cam Ranh'},
+  {label: 'Da Nang', city: 'Da Nang', value: 'Da Nang'},
+  {label: 'Phu Bai', city: 'Hue', value: 'Phu Bai'}
+]
+
+export default forwardRef(LocationBox)
---
+++ travelwala/src/pages/home/components/SimplePopper.js
@@ -0,0 +1,59 @@
+import * as React from 'react';
+import Popper from '@mui/material/Popper';
+import Passengers from "./Passengers";
+import Box from "@mui/material/Box";
+import {useStore} from "./store";
+import {Button, ClickAwayListener} from "@mui/material";
+import AirlineSeatReclineNormalIcon from '@mui/icons-material/AirlineSeatReclineNormal';
+import IconTextField from "../../../components/IconTextField";
+
+export default function SimplePopper() {
+  const [open, setOpen] = React.useState(false);
+  const [state] = useStore();
+  const {adult, child, infant} = state;
+  const [label, setLabel] = React.useState(adult.toString().concat(" Adult, ", child.toString(), " Child, ", infant.toString(), " Infant"));
+  let anchorElRef = React.useRef(null)
+
+  const handleFocus = () => {
+    setOpen(true)
+  };
+
+  const handleClickAway = () => {
+    setOpen(false)
+  }
+
+  const id = open ? 'simple-popper' : undefined;
+
+  return (<div>
+    <h2>No. of Passengers</h2>
+    <div ref={anchorElRef}>
+      <IconTextField
+        aria-describedby={id}
+        onFocus={handleFocus}
+        label='Passengers'
+        value={label}
+        sx={{width: 400}}
+        InputProps={{
+          readOnly: true,
+        }}
+        iconStart={<AirlineSeatReclineNormalIcon sx={{color: "#2196f3", fontSize: 25}}/>}
+      />
+    </div>
+
+    <ClickAwayListener onClickAway={(e) => {
+      if (!anchorElRef.current.contains(e.target)) {
+        handleClickAway()
+      }
+    }}>
+      <Popper id={id} open={open} anchorEl={anchorElRef.current}>
+        <Box sx={{border: 1, p: 1, bgcolor: 'white', marginTop: 2, width: 380}}>
+          <Passengers f={(label) => {
+            setLabel(label)
+          }}
+          />
+          <Button color='success' sx={{marginLeft: 39}} onClick={handleClickAway}>Done</Button>
+        </Box>
+      </Popper>
+    </ClickAwayListener>
+  </div>);
+}
