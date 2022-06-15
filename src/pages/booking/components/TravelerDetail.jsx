064f6031392a352d358ace2b44b158262a14efbd
tranthang <ducthang.280401@gmail.com>
2022-06-15 16:59:58 +0700

    Finishing remove passenger detail contact form function

--- src/pages/booking/components/TravelerDetail.jsx
+++ src/pages/booking/components/TravelerDetail.jsx
@@ -6,15 +6,24 @@ import TextField from '@mui/material/TextField';
 import { Container} from '@mui/system';
 
 const TravelerDetailComponent = (props) => {
-    var myform = [];
+    const [title, setTitle] = React.useState("");
+    const [firstName, setFirstName] = React.useState("");
+    const [lastName, setLastName] = React.useState("");
+    const [dateOfBirth, setDateOfBirth] = React.useState("");
+    const [email, setEmail] = React.useState("");
 
-    const removePassenger = (index) => {
-        myform = myform.slice(index);
-        props.setNumberOfPassenger(props.numberOfPassenger-1);
-        console.log(myform);
+    const handleOnChange = (event, setFunction) => {
+        setFunction(event.target.value);
     }
 
-    const detailForm = (index) => {
+    React.useEffect(() => {
+        setTitle(props.passenger.title);
+        setFirstName(props.passenger.firstName);
+        setLastName(props.passenger.lastName);
+        setDateOfBirth(props.passenger.dateOfBirth);
+        setEmail(props.passenger.email);
+    }, [props.passenger]);
+
         return (       
         <div>
             <Box
@@ -45,8 +54,13 @@ const TravelerDetailComponent = (props) => {
                                 lineHeight: 0
                             }}
                         >
-                            <h3>Passenger {index}</h3>
-                            <Button onClick={() => props.numberOfPassenger > 1 ? removePassenger(index-1) : 0 }>Remove Passenger</Button>
+                            <h3>Passenger {props.index+1}</h3>
+                            <Button onClick={() => 
+                                props.onRemove(props.index)
+                                }
+                            >
+                                Remove Passenger
+                            </Button>
                         </Box>
                     </div>
                     <div>
@@ -54,6 +68,11 @@ const TravelerDetailComponent = (props) => {
                             id="title"
                             placeholder="Title"
                             helperText=""
+                            value={title}
+                            onChange={(event) => {
+                                handleOnChange(event, setTitle); 
+                                props.passenger.title = event.target.value;
+                            }}
                         />
                     </div>
                     <div>
@@ -61,11 +80,21 @@ const TravelerDetailComponent = (props) => {
                             id="first-name"
                             placeholder="First Name*"
                             helperText=""
+                            value={firstName}
+                            onChange={(event) => {
+                                handleOnChange(event, setFirstName); 
+                                props.passenger.firstName = event.target.value;
+                            }}
                         />
                         <TextField
                             id="last-name"
                             placeholder="Midle & Last Name*"
                             helperText=""
+                            value={lastName}
+                            onChange={(event) => {
+                                handleOnChange(event, setLastName); 
+                                props.passenger.lastName = event.target.value;
+                            }}
                         />
                     </div>
                     <div>
@@ -73,55 +102,84 @@ const TravelerDetailComponent = (props) => {
                             id="date-of-birth"
                             placeholder="Date of Birth*"
                             helperText=""
+                            value={dateOfBirth}
+                            onChange={(event) => {
+                                handleOnChange(event, setDateOfBirth); 
+                                props.passenger.dateOfBirth = event.target.value;
+                            }}
                         />
                         <TextField
                             id="email-address"
                             placeholder="Email address*"
                             helperText=""
+                            value={email}
+                            onChange={(event) => {
+                                handleOnChange(event, setEmail); 
+                                props.passenger.email = event.target.value;
+                            }}
                         />
                     </div>
                 </Container>
             </Box>
         </div>
         );
-    };
-
-    for (let i = 0 ; i< props.numberOfPassenger; i++) {
-        myform.push(detailForm(i+1));
-    }
-
-    return (
-        <>
-            {myform}
-        </>
-    );
+    // };
 }
 
 
 
 export default function TravelerDetail() {
-    const [numberOfPassenger, setNumberOfPassenger] = React.useState(1);
-
-    React.useEffect(() => {
-    }, [numberOfPassenger]);
+    const [passengers, setPassengers] = React.useState([]);
 
     return (
         <>
         <div>
-            <TravelerDetailComponent numberOfPassenger={numberOfPassenger} setNumberOfPassenger={setNumberOfPassenger}/>
+            {
+                passengers.map((passenger, index) =>
+                    <>
+                        <TravelerDetailComponent
+                            passenger={passenger}
+                            index={index}
+                            onRemove={(index) => 
+                                setPassengers([...passengers.slice(0, index), ...passengers.slice(index+1, passengers.length)])
+                            }
+                            passengers={passengers}
+                        />
+                    </>
+                )
+            }
         </div>
         <div>
-            <Box sx = {{
-                mt: 0,
-                display: 'flex',
-                flexDirection: 'row',
-                alignItems: 'center',
-                justifyContent: 'center',
+            <Box                 
+                component="form"
+                sx={{
+                '& .MuiTextField-root': { m: 1, width: '35ch' },
+                border: 1,
+                flexWrap: 'wrap',
+                display: 'inline-flex',
                 alignContent: 'center',
-            }}>
+                pb: 1,
+                mb:5,
+                justifyContent: 'center',
+                ...ThemeStyle.box,
+                }}
+                noValidate
+                autoComplete="off"
+            >
                 <Button 
                     variant='outlined'
-                    onClick={() => setNumberOfPassenger(numberOfPassenger+1)}
+                    onClick={() => {setPassengers(
+                        [
+                            ...passengers ,
+                            {
+                                title: "",
+                                firstName: "",
+                                lastName: "",
+                                dateOfBirth: "",
+                                email: ""
+                            }
+                        ]); 
+                    }}
                     sx={{m:2}}
                 >
                     Add Passenger
