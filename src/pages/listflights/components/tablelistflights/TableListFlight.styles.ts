b3d28293fa10d79a472ec855b3e4df5af822c4fb
Hieu Tran <hieu.trantrung2908@gmail.com>
2022-06-23 15:39:42 +0700

    feature: style flight list table

---
+++ src/pages/listflights/components/tablelistflights/TableListFlight.styles.ts
@@ -0,0 +1,9 @@
+import { Box, styled } from "@mui/material";
+
+export const BoxTableListFlight = styled(Box)({
+  margin: "0px 24px 16px",
+  width: "calc(100% - 50px)",
+  height: "570px",
+  border: "1px solid #F6F6FE",
+  borderRadius: 8,
+});
--- src/pages/listflights/components/tablelistflights/TableListFlights.tsx
+++ src/pages/listflights/components/tablelistflights/TableListFlights.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { SearchFlightResponse } from "../../../../api/flight/FlightService.types";
 import { Table, TableBody, TableContainer } from "@mui/material";
 import FlightItem from "./FlightItem/FlightItem";
+import { BoxTableListFlight } from "./TableListFlight.styles";
 
 type TableListFlightsProps = {
   data: SearchFlightResponse;
@@ -11,15 +12,42 @@ function TableListFlights(props: TableListFlightsProps) {
   const { data } = props;
 
   return (
-    <TableContainer>
-      <Table aria-label="simple table" style={{ borderCollapse: "inherit" }}>
-        <TableBody>
-          {data.departureFlights?.map((flight) => (
-            <FlightItem flight={flight} />
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <BoxTableListFlight
+      sx={{
+        boxShadow: 3,
+      }}
+    >
+      <TableContainer
+        sx={{
+          margin: "0px 16px 8px",
+          width: "calc(100% - 30px)",
+          height: "100%",
+          "&::-webkit-scrollbar": {
+            width: 10,
+          },
+          "&::-webkit-scrollbar-track": {
+            backgroundColor: "#fff",
+          },
+          "&::-webkit-scrollbar-thumb": {
+            background: "#888",
+            borderRadius: 10,
+            border: "2px solid transparent",
+            backgroundClip: "content-box",
+            "&:hover": {
+              backgroundColor: "#555",
+            },
+          },
+        }}
+      >
+        <Table aria-label="simple table" style={{ borderCollapse: "inherit" }}>
+          <TableBody>
+            {data.departureFlights?.map((flight) => (
+              <FlightItem flight={flight} />
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </BoxTableListFlight>
   );
 }
 
