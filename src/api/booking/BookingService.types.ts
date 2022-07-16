155eca6e2f34d46f5d0ea3c695234db63cfe6ee7
tranthang <ducthang.280401@gmail.com>
2022-07-16 17:56:06 +0700

    added booking api

---
+++ src/api/booking/BookingService.types.ts
@@ -0,0 +1,51 @@
+export interface BookingRequest {
+    createBookingFlightSpecs: CreateBookingFlightSpecs
+    bookingContact: BookingContact;
+  }
+
+interface CreateBookingFlightSpecs {
+    travellerSpecs: TravellerSpecs
+    flightProductSpecs: FlightProductSpecs
+}
+
+interface FlightProductSpecs {
+    flightId: String
+    seatClass: String
+    adultCount: Number
+    childCount: Number
+    infantCount: Number
+}
+
+interface TravellerSpecs {
+    adultFormData: TravelerDataForm[]
+    childFormData: TravelerDataForm[]
+    infantFormData: TravelerDataForm[]
+}
+
+interface TravelerDataForm {
+    title: String
+    firstName: String
+    lastName: String
+    dob: DateOfBirth
+    nationality: String
+}
+
+interface DateOfBirth {
+    day: Number
+    month: Number
+    year: Number
+}
+
+interface BookingContact {
+    firstName: String
+    lastName: String
+    phoneNumber: String
+    email: String
+}
+
+export interface CreateBookingResponse {
+    amount: number;
+    currency: string;
+    clientSecret: string;
+}
+  
