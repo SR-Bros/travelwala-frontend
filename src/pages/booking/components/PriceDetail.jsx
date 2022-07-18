aa250a2c63abfe6d10e664221c7a2da8c6ad093f
tranthang <ducthang.280401@gmail.com>
2022-07-18 12:54:11 +0700

    added api request for booking

--- src/pages/booking/components/PriceDetail.jsx
+++ src/pages/booking/components/PriceDetail.jsx
@@ -3,6 +3,11 @@ import * as ThemeStyle from './css/style'
 import Box from '@mui/material/Box';
 import { Container } from '@mui/system';
 import { Divider, Grid, Typography } from '@mui/material';
+import {
+    CreateBookingResponse,
+    BookingRequest,
+  } from "../../../api/booking/BookingService.types";
+  import BookingService from "../../../api/booking/BookingService";
 
 const testPrice = {
     total: 624,
@@ -21,7 +26,8 @@ const testPrice = {
 
 }
 
-export default function PriceDetail(props) {
+export default function PriceDetail() {
+    const [data, setData] = React.useState({});
     const priceDetail = testPrice;
     /*
     const priceDetail = [...props.priceDetail];
@@ -31,6 +37,56 @@ export default function PriceDetail(props) {
         pb: 1,
         fontSize: '20px'
     }
+
+    const loadInvoice = (bookingData) => {
+        BookingService.requestBookingInvoice(bookingData)
+          .then((response) => {
+            if (response && response.data) {
+              setData(response.data);
+            }
+          })
+          .catch((reason) => console.log(reason));
+    };
+
+    React.useEffect(() => {
+        async function init() {
+            const bookingData = await initCriteria();
+            loadInvoice(bookingData);
+          }
+      
+          init();
+    });
+
+    const initCriteria = () => {
+        const passengerList = useSelector();
+        const departureFlight = useSelector();
+        const returnFlight = useSelector();
+        const contactDetail = useSelector();
+
+        return {
+            createBookingFlightSpecs: {
+                travellerSpecs: {
+                    adultFormData: adultData,
+                    childFormData: childData,
+                    infantFormData: infantData
+                },
+                flightProductSpecs: {
+                    departureFlightId: departureFlightInfo,
+                    returnFlightId: returnFlightInfo,
+                    seatClass: 
+                    adultCount:
+                    childCount:
+                    infantCount:
+                }
+            },
+            bookingContact: {
+                firstName: 
+                lastName:
+                phoneNumber:
+                email:
+            }
+        };
+    };
 
     return (
             <Box
